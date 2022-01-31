5530587aad076460044ea94d2f813f35e06180e0
kayteckk <85080651+kayteckk@users.noreply.github.com>
2022-01-31 22:44:33 +0100

    Update testykalkulator_wtb.py

--- testykalkulator_wtb.py
+++ testykalkulator_wtb.py
@@ -4,13 +4,11 @@ class CalculatorTests(unittest.TestCase):
     def test_dodawanie(self):
         l1 = 5
         l2 = 6
-        wynikowa = dodawanie(l1,l2)
-        self.assertEqual(wynikowa,11)
+        assert dodawanie(l1,l2) == 11
     def test_odejmowanie(self):
         l1=-8
         l2=3
-        wynikowa = odejmowanie(l1,l2)
-        self.assertEqual(wynikowa,-11)
+        assert odejmowanie(l1,l2) == -11
     
 
 if __name__ == '__main__':
